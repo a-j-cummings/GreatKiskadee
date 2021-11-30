50d65dc0292e16d6c4ef95148dc33fda1f11077c
a-j-cummings <jared.cummings4@gmail.com>
2021-11-29 20:59:21 -0700

    NIMBLE code that RUNS!!!

--- ppl_project.R
+++ ppl_project.R
@@ -1,21 +1,19 @@
-# Fits latent class Bayesian model via Stan on haplotype data
+# Fits latent class Bayesian model via NIMBLE on haplotype data
 
 library(tidyverse)
-library(rstan)
-library(R2jags)
+library(nimble)
 
 source('preproc.R')
 
 # set up parallel processing
 nCores <- parallel::detectCores()
 options(mc.cores = nCores) # use all available cores
-rstan_options(auto_write = TRUE) # cache compiled code
 
 haplo <- read_csv('haplotype.csv')
 
 # 0. The model as found in CASI
 haplo_preproc0 <- preproc(haplo, dist = 'bernoulli')
-# prepare data objects
+# prepare data objects and constants
 n_classes <- 3
 snp <- haplo_preproc0$preproc_data
 n_copies <- 2
@@ -30,69 +28,58 @@ copy2 <- snp[rep(c(FALSE, TRUE), n_ind),] %>%
 X <- array(dim = c(n_ind, n_loc, n_copies))
 X[,,1] <- copy1
 X[,,2] <- copy2
-ind <- unique(haplo_preproc0$id$ind)
-copy <- haplo_preproc0$id$copy
 alpha_q <- c(1,1,1)
 alpha_p <- c(1,1)
-data0 <- c('n_classes', 'n_copies', 'n_ind', 'n_loc', 'X', 'alpha_q', 'alpha_p')
-model0 <- "
-  model {
-    for (i in 1:n_ind){
-      Q[i,] ~ ddirich(alpha_q)
-      for (m in 1:n_loc){
-        for (c in 1:n_copies){
-          Z[i,m,c,] ~ dmulti(Q[i,], 1)
-          for (j in 1:n_classes){
-            if (Z[i,m,c,j] == 1){
-              X[i,m,c,] ~ dmulti(P[j,m,], 1)
-            }
-          }
-        }
-      }
+# prepare initial values
+set.seed(1)
+Q0 <- t(replicate(n_ind, rdirch(1, alpha_q)))
+Z0 <- array(dim = c(n_ind, n_loc, n_copies, n_classes))
+for (i in 1:n_ind){
+  for (m in 1:n_loc){
+    for (c in 1:n_copies){
+      Z0[i, m, c,] <- rmulti(1, 1, Q0[i,]) 
     }
-    for (j in 1:n_classes){
-      for (m in 1:n_loc){
-        P[j,m,] ~ ddirich(alpha_p)
+  }
+}
+P0 <- aperm(replicate(n_classes, replicate(n_loc, rdirch(1, alpha_p))), c(3,2,1))
+
+model0_code <- nimbleCode({
+  for (i in 1:n_ind){
+    Q[i,1:3] ~ ddirch(alpha_q[1:3])
+    for (m in 1:n_loc){
+      for (c in 1:n_copies){
+        Z[i,m,c,1:3] ~ dmultinom(Q[i,1:3], 1)
+        j[i,m,c] <- 1*(Z[i,m,c,1] == 1) + 2*(Z[i,m,c,2] == 1) + 3*(Z[i,m,c,3] == 1)
+        X[i,m,c] ~ dbinom(P[j[i,m,c],m,1], 1)
       }
     }
   }
-"
-params0 <- c('Q', 'Z', 'P')
-writeLines(model0, 'model0.txt')
+  for (k in 1:n_classes){
+    for (m in 1:n_loc){
+      P[k,m,1:2] ~ ddirch(alpha_p[1:2])
+    }
+  }
+})
 
-sim0 <- jags(data=data0, inits=NULL,
-             parameters.to.save=params0,
-             model.file='model0.txt',
-             n.iter=10, n.burnin=0,
-             n.chains=1, n.thin=1)
+model0_consts <- list(n_classes = n_classes,
+                      n_copies = n_copies,
+                      n_ind = n_ind,
+                      n_loc = n_loc,
+                      alpha_q = alpha_q,
+                      alpha_p = alpha_p)
 
+model0_data <- list(X = X)
 
+model0_inits <- list(Z=Z0,
+                     Q=Q0,
+                     P=P0)
 
+model0 <- nimbleModel(code = model0_code, name = 'model0', 
+                     constants = model0_consts, data = model0_data,
+                     inits = model0_inits)
 
-
-
-
-data0 <- list(n_classes = n_classes,
-              X = X, 
-              n_copies = n_copies,
-              n_ind = n_ind,
-              n_loc = n_loc,
-              ind = ind,
-              copy = copy,
-              alpha_q = alpha_q,
-              alpha_p = alpha_p)
-fit0 <- stan(model_code = readLines('model0.stan'),
-             data = data0,
-             iter = 10,
-             warmup = 0,
-             thin = 1,
-             chains = nCores)
-
-
-
-
-
-# 1. The model with genotype as a multinomial
-
-haplo_preproc1 <- preproc(haplo, dist = 'multinomial')
+mcmc0_out <- nimbleMCMC(code = model0_code, constants = model0_consts, 
+                        data = model0_data, inits = model0_inits, 
+                        nchains = 1, niter = 10, summary = TRUE, 
+                        WAIC=TRUE, monitors = c('Q', 'P', 'Z'))
 
